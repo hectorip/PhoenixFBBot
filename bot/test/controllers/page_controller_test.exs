a5b27703b0221361d350ec01b77c7fe6d61b8ef7
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-05-28 23:59:59 -0700

    Adding phoenix project

---
+++ bot/test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule Bot.PageControllerTest do
+  use Bot.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
