5bb3a15a07759e645b3543f302fe85446432bd07
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-07-15 23:40:54 -0500

    General endpoint

--- web/controllers/facebook_controller.ex
+++ web/controllers/facebook_controller.ex
@@ -6,19 +6,23 @@ defmodule Bot.FacebookController do
     "hub.verify_token" => verify_token,
     "hub.challenge" => challenge})
   do
+    IO.puts(@verify_token)
+    IO.puts(verify_token)
     case verify_token do
       @verify_token -> text conn, challenge
       _ ->             text conn, "WRONG"
     end
   end
   def webhook(conn, _), do: text conn, "I have no idea of how to respond this"
-
-  def handle_message(conn, _params = %{"entry" => entry}) do
-      %{"id" => _, "messaging" => messaging} = hd(entry)
-
-      messaging
-      |> Enum.each(&Bot.MessageHandler.handle/1)
+  def handle_message(conn, _params) do
       text conn, "Responding Message"
   end
+  # def handle_message(conn, _params = %{"entry" => entry}) do
+  #     %{"id" => _, "messaging" => messaging} = hd(entry)
+
+  #     messaging
+  #     |> Enum.each(&Bot.MessageHandler.handle/1)
+  #     text conn, "Responding Message"
+  # end
 
 end
